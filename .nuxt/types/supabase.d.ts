e7e4153793719dd6ceab063b019aad2080e1d01d
yjbarcial <yjbarcial@gmail.com>
2026-07-10 20:13:48 +0800

    Stop tracking .nuxt build cache

--- .nuxt/types/supabase.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '#supabase/server' {
-  const serverSupabaseClient: typeof import('C:/Users/yssah/Documents/GitHub/savings-tracker/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseClient
-  const serverSupabaseServiceRole: typeof import('C:/Users/yssah/Documents/GitHub/savings-tracker/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseServiceRole
-  const serverSupabaseUser: typeof import('C:/Users/yssah/Documents/GitHub/savings-tracker/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseUser
-  const serverSupabaseSession: typeof import('C:/Users/yssah/Documents/GitHub/savings-tracker/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseSession
-}

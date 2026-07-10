c9a7f9cb23402d4f7fe2ad2b51d90f60e3902b13
yjbarcial <yjbarcial@gmail.com>
2026-07-10 20:14:46 +0800

    stop tracking .nuxt build cache

---
+++ .nuxt/types/runtime-config.d.ts
@@ -0,0 +1,72 @@
+import { RuntimeConfig as UserRuntimeConfig, PublicRuntimeConfig as UserPublicRuntimeConfig } from 'nuxt/schema'
+  interface SharedRuntimeConfig {
+   app: {
+      buildId: string,
+
+      baseURL: string,
+
+      buildAssetsDir: string,
+
+      cdnURL: string,
+   },
+
+   nitro: {
+      envPrefix: string,
+   },
+
+   supabase: {
+      serviceKey: any,
+
+      secretKey: any,
+   },
+  }
+  interface SharedPublicRuntimeConfig {
+   supabase: {
+      url: string,
+
+      key: string,
+
+      redirect: boolean,
+
+      redirectOptions: {
+         login: string,
+
+         callback: string,
+
+         exclude: Array<string>,
+
+         cookieRedirect: boolean,
+
+         saveRedirectToCookie: boolean,
+      },
+
+      cookieName: string,
+
+      cookiePrefix: string,
+
+      useSsrCookies: boolean,
+
+      cookieOptions: {
+         maxAge: number,
+
+         sameSite: string,
+
+         secure: boolean,
+      },
+
+      clientOptions: any,
+   },
+  }
+declare module '@nuxt/schema' {
+  interface RuntimeConfig extends UserRuntimeConfig {}
+  interface PublicRuntimeConfig extends UserPublicRuntimeConfig {}
+}
+declare module 'nuxt/schema' {
+  interface RuntimeConfig extends SharedRuntimeConfig {}
+  interface PublicRuntimeConfig extends SharedPublicRuntimeConfig {}
+}
+declare module 'vue' {
+        interface ComponentCustomProperties {
+          $config: UserRuntimeConfig
+        }
+      }
